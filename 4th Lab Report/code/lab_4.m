a8d0de12cdd83476abeff360e26974e2c1612c9c
SOHEL RANA <sohelrana104322@gmail.com>
2025-09-03 08:59:24 +0600

    added lab report 4 code

---
+++ 4th Lab Report/code/lab_4.m
@@ -0,0 +1,38 @@
+clear all;
+close all;
+clc;
+
+sample = [-3, 2, 4, 1, 2, 0, 4, -5, 7];
+n1 = -3:5;
+
+scaled = [];
+n2 = [];
+
+for i = 1:length(n1)
+    x = 2 * n1(i);
+    selected = find(n1 == x);
+    if ~isempty(selected)
+        n2 = [n2, x];
+        scaled = [scaled, sample(selected)]
+    end
+end
+
+n2 = n2 / 2;
+
+subplot(2, 1, 1);
+stem(n1, sample, 'b', 'linewidth', 2);
+title('Original signal');
+xlabel('n');
+ylabel('amplitude');
+grid on;
+xlim([-4, 6]);
+ylim([-6, 8]);
+
+subplot(2, 1, 2);
+stem(n2, scaled, 'b', 'linewidth', 2);
+title('Time scaled signal: x(2n)');
+xlabel('n');
+ylabel('amplitude');
+grid on;
+xlim([-4, 6]);
+ylim([-6, 8]);
